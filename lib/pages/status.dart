835e853557be18979e076ac6aee26c2ca7b4ea0a
Miguel <i1720539@continental.edu.pe>
2021-04-02 14:55:06 -0500

    AppTerminada

---
+++ lib/pages/status.dart
@@ -0,0 +1,34 @@
+import 'package:band_names/services/socket_service.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class StatusPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final socketService = Provider.of<SocketService>(context);
+    // socketService.socket.emit(event)
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text('ServerStatus: ${socketService.serverStatus}')],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.message),
+        onPressed: () {
+          //tarea
+          //emitir:emitir-mensaje
+          //{nombre:'Flutter',mensaje:'Hola desde flutter'}
+          socketService.emit(
+            'emitir-mensaje',
+            {
+              'nombre': 'Flutter',
+              'mensaje': 'Hola desde Flutter',
+            },
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/services/socket_service.dart
@@ -0,0 +1,42 @@
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'package:flutter/material.dart';
+
+enum ServerStatus { Online, Offline, Connecting }
+
+class SocketService with ChangeNotifier {
+  ServerStatus _serverStatus = ServerStatus.Connecting;
+  IO.Socket _socket;
+
+  ServerStatus get serverStatus => this._serverStatus;
+  IO.Socket get socket => this._socket;
+  Function get emit => this.socket.emit;
+
+  SocketService() {
+    this._initConfig();
+  }
+  void _initConfig() {
+    // Dart client
+    this._socket = IO.io('http://192.168.101.41:3000/', {
+      'transports': ['websocket'],
+      'autoConnect': true,
+    });
+    this._socket.onConnect((_) {
+      print('connect');
+      this._serverStatus = ServerStatus.Online;
+      notifyListeners();
+    });
+
+    this._socket.onDisconnect((_) {
+      print('disconnect');
+      this._serverStatus = ServerStatus.Offline;
+      notifyListeners();
+    });
+
+    // socket.on('nuevo-mensaje', (payload) {
+    //   print('nuevo-mensaje:');
+    //   print('nombre:' + payload['nombre']);
+    //   print('mensaje:' + payload['mensaje']);
+    //   print(payload.containsKey('mensaje2') ? payload['mensaje2'] : 'no hay ');
+    // });
+  }
+}
